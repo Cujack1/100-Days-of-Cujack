afe5a4c14a6d1e5634342919dedd6cab8cf97da6
Cujack <cujackxmanxmythxlegend@gmail.com>
2022-07-15 19:28:16 -0500

    starting 100 Days Coding Journal

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("upload-btn").addEventListener("click", displayText);
+
+const files = document.getElementById("file-upload").file[0];
+
+function displayText() {
+    let reader = new FileReader();
+
+}
